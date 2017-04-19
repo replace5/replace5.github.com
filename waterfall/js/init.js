235cdea85f041d114bdb7649450ee0ee7531cd80
坚壳 <822481516@qq.com>
2017-04-19 15:55:32 +0800

    Update init.js

--- waterfall/js/init.js
+++ waterfall/js/init.js
@@ -1,25 +1,29 @@
 factory(function() {
     return function (waterfall, ajax) {
-
         var container = document.getElementById("test");
-
-        for (var i = 0, lis = container.children; i < lis.length; i++) {
-            var li = lis[i];
-            var width = (li.getAttribute('data-width') || 0) + 'px';
-            var height = (li.getAttribute('data-height') || 0) + 'px';
-            li.style.width = width
-            li.style.height = height;
-
-            li.innerHTML = ReferrerKiller.imageHtml(li.getAttribute("data-src"), {
-                style: 'width: ' + width + ';height: '+ height
-            });
-        }
-
-        var wt = waterfall({
+        var options = {
             padding: 10,
             cloumn_width: 250,
             container: container
-        });
+        };
+        var cloumn_padding = 10;
+
+        for (var i = 0, lis = container.children; i < lis.length; i++) {
+            var li = lis[i];
+            var width = li.getAttribute('data-width') || options.cloumn_width;
+            var height = li.getAttribute('data-height') || 150;
+            var real_width = options.cloumn_width - cloumn_padding * 2;
+            var real_height = real_width * height / width;
+
+            li.style.width = real_width + 'px';
+            li.style.height = real_height + 'px';
+
+            li.innerHTML = ReferrerKiller.imageHtml(li.getAttribute("data-src"), {
+                style: 'width: ' + real_width + 'px;height: '+  real_height + 'px;'
+            });
+        }
+
+        var wt = waterfall(options);
         window.onscroll = function() {
             var clientHeight = document[document.compatMode === 'CSS1Compat' ? "documentElement" : "body"].clientHeight,
                 scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop),
@@ -39,8 +43,8 @@ factory(function() {
                             for (var i = 0, len = data.items.length; i< len; i ++) {
                                 if (Math.random() > 0.4) {continue;}
                                 var item = data.items[i];
-                                var width = 280 + 'px';
-                                var height = item.height*280/item.width + 'px';
+                                var width = (options.cloumn_width - cloumn_padding) + 'px';
+                                var height = item.height*(options.cloumn_width - cloumn_padding)/item.width + 'px';
                                 var li = document.createElement('li');
                                 li.style.width = width + 'px';
                                 li.style.height = height + 'px';
