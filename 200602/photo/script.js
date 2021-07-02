efca00992dca02163ab08ec9003d557de10b7ebb
陈璐 <chenlu@sunteng.com>
2021-07-02 19:59:57 +0800

    Update: photo

--- 200602/photo/script.js
+++ 200602/photo/script.js
@@ -1,19 +1,19 @@
-const fs = require("fs");
+const fs = require("fs")
 
-const ext = ["jpg", "jpeg", "png", "gif"];
+const ext = ["jpg", "jpeg", "png", "gif"]
 
 fs.readdir("./", (err, files) => {
-  const images = [];
+  const images = []
   files.forEach((file) => {
     if (ext.includes(file.split(".").pop())) {
-      images.push(file);
+      images.push(file)
     }
   })
 
-  fs.readFile("./tpl.html", "utf8" ,(err, content) => {
-    content = content.replace(/'{images}'/, JSON.stringify(images));
+  fs.readFile("./tpl.html", "utf8", (err, content) => {
+    content = content.replace(/'{images}'/, JSON.stringify(images))
     fs.writeFile("./index.html", content, "utf8", () => {
-      console.log("end");
-    });
+      console.log("生成完毕: ", images.length)
+    })
   })
 })
